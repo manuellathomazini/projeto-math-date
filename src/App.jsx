c1ca9b8134f9f7c518ca645497b0ec06952fc851
Manuella S. Thomazini <manuellathomzn@gmail.com>
2026-09-16 22:03:47 -0300

    feat: backend do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  // Estatos
+  const [randomNumber, setRandomNumber] = useState(null);
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const generateNumber = () => {
+    const num = Math.floor(Math.random() * 100 + 1);
+    setRandomNumber(num);
+  };
+
+  const formattedTime = currentTime.toLocaleTimeString();
+
   return (
     <div>
-      
     </div>
   )
 }
