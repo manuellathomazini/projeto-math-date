3bbd9d0471dac301e21efc47cbb318fbb498f59b
Manuella S. Thomazini <manuellathomzn@gmail.com>
2026-09-16 22:04:51 -0300

    feat(style): frontend do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,19 @@ const App = () => {
   const formattedTime = currentTime.toLocaleTimeString();
 
   return (
-    <div>
+    <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-100 to-purple-200'>
+      <div className='bg-white p-8 rounded-xl shadow-lg w-full max-w-md text-center'>
+        <h1 className='text-2xl font-bold mb-6'>Explorando Math e Date</h1>
+        <div className='mb-6'>
+        <h2 className='text-lg font-semibold text-gray-700'>⏰ Relógio atual</h2>
+        <p className='text-3xl font-mono mt-2'>{formattedTime}</p>
+        </div>
+        <div className='mb-6 items-center justify-center'>
+          <h2 className='text-lg font-semibold text-gray-700'>🪄Número Aleatório</h2>
+          {randomNumber !== null && (<p className='text-3xl font-bold text-gray-600 mt-2'>{randomNumber}</p>)}
+          <button onClick={generateNumber} className='mt-4 bg-blue-500 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-700 transition-colors'>Gerar Número</button>
+        </div>
+      </div>
     </div>
   )
 }
